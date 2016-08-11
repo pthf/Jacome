ae61ece658a77b573e0032eabc1e89a60421692f
Diego Fuentes <DiegoFu@MacBook-Air-de-Diego.local>
2016-08-11 18:15:23 -0500

    filtro en categorias de productos

--- js/app.js
+++ js/app.js
@@ -6,24 +6,30 @@
     	'jacomeSite.directives',
 			'ngMap'
   	]);
-
-	app.run(function($rootScope, $anchorScroll){
-		//Scroll hacia arriba cada vez que cambie de ruta
-		$rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
-		    window.scrollTo(0, 0);
-		});
-	})
+	// 
+	// app.run(function($rootScope, $anchorScroll){
+	// 	//Scroll hacia arriba cada vez que cambie de ruta
+	// 	$rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
+	// 	    window.scrollTo(0, 0);
+	// 	});
+	// })
 
 	app.config(['$routeProvider', function ($routeProvider) {
 
 		$routeProvider
 			.when('/', {
 				templateUrl: 'views/home.html',
-				controller: 'homeSliderController'
+				controller: 'homeController'
 			})
 			.when('/productos', {
 				templateUrl: 'views/productos-view.html',
 				controller: 'homeController'
+			})
+			.when('/productos/:categoria/:subcategoria', {
+				templateUrl: 'views/productos-view.html',
+			})
+			.when('/productos/:categoria', {
+				templateUrl: 'views/productos-view.html',
 			})
 			.when('/equipos', {
 				templateUrl: 'views/equipos.html',
@@ -37,13 +43,14 @@
 				templateUrl: 'views/cotizador.html',
 				controller: 'homeController'
 			})
-			.when('/productos/product-item', {
+			.when('/product-item', {
 				templateUrl: 'views/product-item.html',
 				controller: 'homeController'
 			})
 			.otherwise({
 				redirectTo: '/'
 			});
+
 	}]);
 
 })();
--- js/controllers.js
+++ js/controllers.js
@@ -3,28 +3,11 @@
 	angular.module('jacomeSite.controllers', [])
 
 		.controller('homeController', ['$scope', function($scope){
+
 		}])
 
-		.controller('homeSliderController', ['$scope', function($scope){
-				$('.logo-header svg').on('click', function(){
-					$('li.option-item a').removeClass('selected');
-				})
+		.controller('itemSelectedCheckController', ['$scope', '$routeParams', function($scope, $routeParams){
+			$scope.itemSelected = $routeParams.categoria;
 		}])
-
-		//resaltar la opcion del menu resaltado
-		.controller('itemSelectedController', ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location ){
-			$scope.routeParams = $location.path();
-			switch ($scope.routeParams) {
-				case '/': $scope.itemselected = 0;  break;
-				case '/productos': $scope.itemselected = 1;  break;
-				case '/equipos': $scope.itemselected = 2;  break;
-				case '/contacto': $scope.itemselected = 3;  break;
-				case '/cotiza': $scope.itemselected = 4;  break;
-			}
-			$scope.changerute = function(item){
-				$scope.itemselected = item;
-			}
-		}])
-
 
 })();
